9f1b3ccd515fb73e2fec23392a1dff3be8f2d6b1
bellab8201 <59892760+bellab8201@users.noreply.github.com>
2021-11-03 11:33:19 -0600

    Update vigenere.py

--- vigenere.py
+++ vigenere.py
@@ -2,6 +2,8 @@
 
 import sys
 from collections import Counter
+import operator
+import itertools
 
 #taken from Wikipedia
 letter_freqs = {
@@ -35,12 +37,40 @@ letter_freqs = {
 
 alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-
 def pop_var(s):
     """Calculate the population variance of letter frequencies in given string."""
     freqs = Counter(s)
     mean = sum(float(v)/len(s) for v in freqs.values())/len(freqs)  
     return sum((float(freqs[c])/len(s)-mean)**2 for c in freqs)/len(freqs)
+def decrypt(s,key):
+    cipher_decrypt =[]
+    for x in range(len(s)):
+        val = (ord(s[x]) - ord(key[x%len(key)])) % 26
+        val += ord('A')
+        cipher_decrypt.append(chr(val))
+    return  "".join(cipher_decrypt)
+def match_score(d_string):
+    smallest = 100
+    final_letter = ""
+    possible_combos=[]
+    c = Counter(d_string)
+    c = c.most_common()[:3:1]
+    for x in range(len(c)):
+        possible_combos.append(c[x][0])
+        num = c[x][1]
+        if num !=0:
+            l_freq = num/len(d_string)
+            difference = abs(l_freq-letter_freqs[c[x][0]])
+            if difference < smallest:
+                smallest = difference
+                final_letter = c[x][0]
+    return smallest, final_letter, possible_combos
+
+
+
+
+
+
 
 
 
@@ -48,6 +78,45 @@ if __name__ == "__main__":
     # Read ciphertext from stdin
     # Ignore line breaks and spaces, convert to all upper case
     cipher = sys.stdin.read().replace("\n", "").replace(" ", "").upper()
+    #cipher = sys.argv[1].replace("\n", "").replace(" ", "").upper()
+    check = {}
+    repeat = {}
+    count = {}
+    freq = {}
+    test=[]
+    possible={}
+    final = ""
+    n = 7
+    #find each substring of suspected key length, also find the distance between repeats
+    for i in range(0,len(cipher)):
+        count[cipher[i:i+n]] = cipher.count(cipher[i:i+n])
+        dist = cipher.rfind(cipher[i:i+n]) - cipher.find(cipher[i:i+n])
+        if cipher[i:i+n] not in repeat.keys() and dist != 0:
+            repeat[cipher[i:i+n]] = [dist]
+        elif dist != 0:
+            repeat[cipher[i:i + n]].append(dist)
+    for k in range (0,n):
+        check[k] = cipher[k::n]
+    for word in check:
+        for letter in alphabet:
 
-    #################################################################
-    # Your code to determine the key and decrypt the ciphertext here
+            freq[letter] = check[word].count(letter)
+        test.append(sorted(freq.items(),key=operator.itemgetter(1), reverse = True))
+        freq = {}
+
+    repeat = sorted(repeat.items(), key =operator.itemgetter(1),reverse=True)
+    best_guess_lower=0
+    best_key=""
+
+    possible_combos = []
+    for x in check:
+        final += match_score(decrypt(check[x],"E"))[1]
+        possible_combos.append(match_score(decrypt(check[x],"E"))[2])
+    combos = list(itertools.product(*possible_combos))
+    for combo in combos:
+        best_guess = pop_var(decrypt(cipher,combo))
+        if best_guess > best_guess_lower:
+            best_guess_lower = best_guess
+            best_key = combo
+    checker = decrypt(cipher, "".join(best_key) )
+    print("".join(best_key))
