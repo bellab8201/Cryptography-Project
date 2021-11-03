44affed893e85cfda78cfcab09e25725ede899ef
bellab8201 <59892760+bellab8201@users.noreply.github.com>
2021-11-03 11:46:13 -0600

    Update len_ext_attack.py

--- len_ext_attack.py
+++ len_ext_attack.py
@@ -24,9 +24,38 @@ if __name__ == '__main__':
     # Get url from command line argument (argv)
     url = sys.argv[1]
 
+    #url = 'https://project1.ecen4133.org/autograder/lengthextension/api?token=f1bb7d2c97b5bfb207bc2fcaf961e76c&command=Test1&command=GradeProject&command=NoOp'
+
     #################################
     # Your length extension code here
+    url_parse = urlparse(url) 
+
+
+    token = url_parse.query # This is the portion of the URL from 'token=' and onwards
+    scheme = url_parse.scheme
+    netloc = url_parse.netloc
+    path = url_parse.path
+
+
+    commands = token.split('&', 1)
+    concat_commands = commands[1]+"&command=UnlockSafes"
 
 
 
+    length_of_m = len(commands[1]) + 8
+    bits = (length_of_m + len(padding(length_of_m*8)))*8
 
+    original_token = token.split('&')[0]
+    original_token = original_token.split('=')[1]
+
+    
+    pad = quote(padding(length_of_m*8))
+    
+    # Initialize the hash object
+    h = md5(state=bytes.fromhex(original_token), count=bits)
+    # Compression function appending UnlockSafes to the original token
+    h.update("&command=UnlockSafes")
+    # Generate new token that includes the command UnlockSafes
+    new_token = h.hexdigest()
+    # Add padding to url in order to ...
+    print(scheme + "://" + netloc + path + "?" + "token=" + new_token + "&" + commands[1] + pad + "&command=UnlockSafes")
